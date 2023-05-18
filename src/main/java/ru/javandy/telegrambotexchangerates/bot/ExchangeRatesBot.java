f16bcf48d98cd0bd8e402ba33f6552331d85de87
JavAndy58 <digital.designs.russia@gmail.com>
2023-05-18 21:53:04 +0300

    init Bot

--- src/main/java/ru/javandy/telegrambotexchangerates/bot/ExchangeRatesBot.java
+++ src/main/java/ru/javandy/telegrambotexchangerates/bot/ExchangeRatesBot.java
@@ -2,20 +2,34 @@ package ru.javandy.telegrambotexchangerates.bot;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
-public class ExchangeRatesBot {
+public class ExchangeRatesBot implements UpdatesListener {
     private final TelegramBot bot;
 
-    public void serve() {
-        bot.setUpdatesListener(updates -> {
-            updates.forEach(System.out::println);
 
-            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    @PostConstruct
+    public void init() {
+        bot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            updates.forEach(System.out::println);
+            bot.execute(new SendMessage(update.message().chat().id(), "Приветус"));
+
         });
+
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
 }
